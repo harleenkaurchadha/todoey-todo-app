479b8adb45e8630161ff391800d025f8bfcb62f9
Harleen Kaur <harleen198.chadha@gmail.com>
2021-02-27 00:10:50 +0530

    making dynamic to do list

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 import '../widgets/task_tile.dart';
+import '../models/task.dart';
 
-class TaskList extends StatelessWidget {
-   @override
+class TaskList extends StatefulWidget {
+
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [
+    Task(name: 'buy milk'),
+    Task(name: 'buy bread'),
+    Task(name: 'buy cheese'),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],);
+    return ListView.builder(
+        itemBuilder: (context, index) {
+          return TaskTile(
+            taskTitle: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            checkboxCallback: (checkboxState) {
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            },
+          );
+        },
+        itemCount: tasks.length,
+    );
   }
 }
